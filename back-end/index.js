3cf19b58bfd749c284549be304a80e4b4ce440f4
marcos.asdes <marco.engc@poli.ufrj.br>
2022-05-20 04:56:22 -0300

    added /messages

--- back-end/index.js
+++ back-end/index.js
@@ -66,6 +66,74 @@ async function getParticipants(req, res) {
 }
 /*  */
 
+/*  */
+
+app.post('/messages', sendMessages);
+app.get('/messages', getMessages);
+
+async function sendMessages(req, res){
+    const message = req.body; // { to, text, type }
+    const { user } = req.headers; // from
+    const messageSchema = joi.object({
+        to: joi.string().required(),
+        text: joi.string().required(),
+        type: joi.string().valid('message', 'private_message').required()
+    });
+    const { error } = messageSchema.validate(message, {abortEarly: false}); // abortEarly: false -> in case of error, wait for all variables to be checked before joi accuses an error
+    if (error) {
+        return res.status(422).send(error.details.map(detail => detail.message));
+    }
+    try {
+        const participant = await database.collection('participants').findOne({name: user});
+        if (!participant) {
+            return res.sendStatus(422);
+        }
+        const { to, text, type } = message;
+        await database.collection("messages").insertOne({
+          to,
+          text,
+          type,
+          from: user,
+          time: dayjs().format('HH:mm:ss')
+        });
+    
+        res.sendStatus(201);
+
+    } catch (error) {
+        console.log(error);
+        res.status(422).send({
+            message: 'User does not exist',
+            detail: error
+        });
+    }
+}
+
+async function getMessages(req, res){
+    const { user } = req.headers; //const user = req.header('User');
+    try {
+        const filteredMessages = await database.collection('messages').find({
+            $or: [ 
+                { type: 'message'}, 
+                { $or: [
+                    { to: 'Todos' }, 
+                    { to: user }, 
+                    { from: user } 
+                    ] 
+                } 
+            ]
+        }).limit(50).toArray();
+        res.send(filteredMessages);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            message: 'Error getting messages',
+            detail: error
+        });
+    }
+}
+
+/*  */
+
 app.get('/', (_req, res) => {
 	res.send('Online');
 });
